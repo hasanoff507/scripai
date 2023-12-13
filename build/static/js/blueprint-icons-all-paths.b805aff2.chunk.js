94718e3aec01bcde40d19f4b3150856353329ba3
hasanoff507 <hasanoff507@gmail.com>
2023-12-13 15:42:00 +0500

    some changes

--- build/static/js/blueprint-icons-all-paths.b805aff2.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[2],{214:function(n,t,r){"use strict";r.r(t),r.d(t,"getIconPaths",(function(){return u})),r.d(t,"iconNameToPathsRecordKey",(function(){return i}));var c=r(199),a=r(212);r.d(t,"IconSvgPaths16",(function(){return a}));var e=r(213);r.d(t,"IconSvgPaths20",(function(){return e}));var o=r(56);function u(n,t){var r=Object(c.a)(n);return t===o.a.STANDARD?a[r]:e[r]}function i(n){return Object(c.a)(n)}}}]);
-//# sourceMappingURL=blueprint-icons-all-paths.b805aff2.chunk.js.map
